d82d1f94a19aae7f4b746cd0cd87005f2819a35b
Eric <e.pennecot@laposte.net>
2022-09-18 22:44:28 +0200

    occurences of GeoDemande renamed geo-demande

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,15 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'GeoDemande'`, async(() => {
+  it(`should have as title 'geo-demande'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('GeoDemande');
+    expect(app.title).toEqual('geo-demande');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to GeoDemande!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to geo-demande!');
   }));
 });
